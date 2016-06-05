6edd0c8eb23e1af853fc3145cdf670ed2590d15a
alex16nba <alexandru16@yahoo.com>
2016-06-06 00:07:53 +0300

    pagina de meniu

--- public/controllers/admin.controller.js
+++ public/controllers/admin.controller.js
@@ -19,7 +19,6 @@
       var url = '/api/categories';
       Util.get(url)
         .then(function success(result) {
-          console.log('get result', result);
           vm.categories = result.data
           vm.selectedCategory = vm.categories[0];
         }, function error(err) {
@@ -28,12 +27,10 @@
     }
 
     vm.addCategory = function() {
-      console.log(vm.category);
       var url = '/api/category';
       Util.create(url, vm.category)
         .then(function success(result) {
           vm.category = {};
-          console.log('result', result);
         }, function error(err) {
           console.log('err', err);
         });
@@ -41,13 +38,10 @@
 
     vm.addProduct = function() {
       var url = '/api/product';
-      console.log(vm.selectedCategory);
       vm.product.category = vm.selectedCategory._id;
-      console.log('vm.product', vm.product);
       Util.create(url, vm.product)
         .then(function success(result) {
           vm.product = {};
-          console.log('result', result);
         }, function error(err) {
           console.log('err', err);
         });
--- public/controllers/meniu.controller.js
+++ public/controllers/meniu.controller.js
@@ -5,13 +5,46 @@
   .module('cantina')
   .controller('MeniuController', MeniuController);
 
-  MeniuController.$inject = ['$state', '$scope'];
+  MeniuController.$inject = ['Util', '$state', '$scope', '$filter'];
 
-  function MeniuController($state, $scope) {
+  function MeniuController(Util, $state, $scope,$filter) {
     var vm = this;
+    vm.products = [];
+    initialize();
 
-    vm.navigateTo = function(state) {
-      $state.go(state);
-    };
+    function initialize() {
+      var url = '/api/products';
+      Util.get(url)
+        .then(function success(result) {
+          sortProductsByCategories(result.data);
+        }, function error(err) {
+          console.log('err', err);
+
+        });
+    }
+
+    function sortProductsByCategories(result) {
+      var test = {};
+      for(var i = 0 ; i < result.length; i++) {
+        var data = result[i];
+        var product = {
+          name: data.name,
+          price: data.price
+        };
+        var categoryName = data.category.name;
+        if(i == 0) {
+          test[categoryName] = [];
+          test[categoryName].push(product);
+        } else {
+          if(test[categoryName]) {
+            test[categoryName].push(product);
+          } else {
+            test[categoryName] = [];
+            test[categoryName].push(product)
+          }
+        }
+      }
+      vm.products = test;
+    }
   }
 })();
